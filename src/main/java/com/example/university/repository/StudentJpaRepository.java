96818739c59273eec5f902ea642cda903043b032
bhavya <bhavya211991@gmail.com>
2024-01-15 11:52:21 +0000

    solution

---
+++ src/main/java/com/example/university/repository/StudentJpaRepository.java
@@ -0,0 +1,25 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * import org.springframework.stereotype.Repository;
+ * 
+ */
+
+// Write your code here
+
+package com.example.university.repository;
+
+
+import com.example.university.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+
+@Repository
+public interface StudentJpaRepository extends JpaRepository<Student,Integer> {
+}
---
+++ src/main/java/com/example/university/repository/StudentRepository.java
@@ -0,0 +1,38 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import java.util.ArrayList;
+ * 
+ */
+
+// Write your code here
+
+package com.example.university.repository;
+
+
+import com.example.university.model.Course;
+import com.example.university.model.Student;
+
+
+import java.util.List;
+
+
+public interface StudentRepository {
+    List<Student> getStudents();
+
+
+    Student getStudentById(int studentId);
+
+
+    Student addStudent(Student studentId);
+
+
+    Student updateStudent(int studentId, Student student);
+
+
+    void deleteStudent(int studentId);
+
+
+    List<Course> getStudentCourses(int studentId);
+}
